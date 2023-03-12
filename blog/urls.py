1bec8401e66cd8a1bfe63d0cb37ff4e7e78207fa
SynapticLad <sachin68834@gmail.com>
2023-03-12 22:41:30 +0530

    Added some URLs

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.home, name='blog-home'),    #always give clear names: from where to where it is directing Example: we are going to home of blog page
+    path('about/', views.about, name='blog-about'), #this means that the requests we have got from urls.py file from project will be directed here
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ from django.http import HttpResponse
 #function to handle traffic from home page
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
